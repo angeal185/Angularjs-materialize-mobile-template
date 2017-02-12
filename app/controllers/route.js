003699914231853788b7737b5cf8d68fd3ca84ea
Angeal185 <beneaves01@hotmail.com>
2017-02-12 14:05:26 +0800

    Add files via upload

---
+++ app/controllers/route.js
@@ -0,0 +1,119 @@
+'use strict';
+
+angular.module('myApp.home', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'views/home.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/about', {
+    templateUrl: 'views/about.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/blog', {
+    templateUrl: 'views/blog.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/blog-single', {
+    templateUrl: 'views/blog-single.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/portfolio', {
+    templateUrl: 'views/portfolio.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/portfolio-single', {
+    templateUrl: 'views/portfolio-single.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/checkout', {
+    templateUrl: 'views/checkout.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/contact', {
+    templateUrl: 'views/contact.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/error404', {
+    templateUrl: 'views/error404.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/pricing', {
+    templateUrl: 'views/pricing.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/register', {
+    templateUrl: 'views/register.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/send-mail', {
+    templateUrl: 'views/send-mail.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/shop', {
+    templateUrl: 'views/shop.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/shop-single', {
+    templateUrl: 'views/shop-single.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/team', {
+    templateUrl: 'views/team.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.controller('homeCtrl', [function() {
+
+}])
---
+++ app/static/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+$("#fakeLoader").fakeLoader({
+        zIndex: 999,
+        spinner: "spinner1",
+        bgColor: "#232323"
+    })
+});
+$(document).ready(function(){
+    $('.slider').slider();
+    $('.materialboxed').materialbox();
+    $('.collapsible').collapsible();
+});
